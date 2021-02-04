a73c801e9997d363ff3fb67882e6c89cbbdb86ff
Kev <kev@quirk.cc>
2021-02-04 09:54:16 +0000

    Added excerpt

--- template-parts/content.php
+++ template-parts/content.php
@@ -13,7 +13,9 @@
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<p class="sub-heading"><?php echo the_excerpt(); ?></p>
+		<?php
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
