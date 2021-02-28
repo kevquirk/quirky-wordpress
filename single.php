e7f42d00954379e760641baf30fcba362fcb0030
Kev <kev@quirk.cc>
2021-02-28 18:15:55 +0000

    Changed typo

--- single.php
+++ single.php
@@ -55,7 +55,7 @@ get_header();
 
 				<p>I don't have comments on this site as they're difficult to manage and take up too much time. I'd rather concentrate on producing content than managing comments.</p>
 
-				<p>Instead of leaving a comment, you could 📝 <a href="/guestbook">sign my guestbook</a> or ✉️ <a href="/contact">contact me</a> instead.
+				<p>Instead of leaving a comment, you can 📝 <a href="/guestbook">sign my guestbook</a> or ✉️ <a href="/contact">contact me</a>.
 			</div>
 
 
