7b616f8d18b995294dedffba2caaa129b3dae84e
Kev Quirk <kev@quirk.cc>
2021-03-31 12:28:47 +0100

    s

--- functions.php
+++ functions.php
@@ -232,3 +232,30 @@ function deregister_google_font() {
     wp_register_style( 'wp-editor-font', get_stylesheet_directory() . '/an-empty.css' );
 }
 add_action( 'admin_enqueue_scripts', 'deregister_google_font', 11 );
+
+// Add widget area to Footer
+function register_widget_areas() {
+
+  register_sidebar( array(
+    'name'          => 'Footer area one',
+    'id'            => 'footer_area_one',
+    'description'   => 'This widget area discription',
+    'before_widget' => '<section class="footer-area footer-area-one">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>',
+  ));
+
+  register_sidebar( array(
+    'name'          => 'Footer area two',
+    'id'            => 'footer_area_two',
+    'description'   => 'This widget area discription',
+    'before_widget' => '<section class="footer-area footer-area-two">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h4>',
+    'after_title'   => '</h4>',
+  ));
+
+}
+
+add_action( 'widgets_init', 'register_widget_areas' );
