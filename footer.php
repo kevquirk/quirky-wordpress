79bddddfb8dbb9dc3f9e8f638c0334df5fa61100
Kev <kev@quirk.cc>
2021-04-01 14:26:25 +0100

    sdf

--- footer.php
+++ footer.php
@@ -32,10 +32,7 @@
 					<p><a taget="blank" href="https://512kb.club"><span class="kb-club-no-bg">512KB Club</span><span class="kb-club-bg">Orange Team</span></a></p>
 				</div>
 
-			<p><a class="top-link" href="#top">Back to top
-<svg xmlns="http://www.w3.org/2000/svg">
-    <path d="M7.406 15.422l-1.406-1.406 6-6 6 6-1.406 1.406-4.594-4.594z"></path>
-</svg></a></p>
+			<p><a class="top-link" href="#top">Back to top ^</a></p>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
