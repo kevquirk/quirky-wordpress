981fe7e5361b7af2f410588ce62ed863e7b1ccc6
Kev <kev@quirk.cc>
2021-04-01 15:37:09 +0100

    dgf

--- template-parts/content.php
+++ template-parts/content.php
@@ -20,7 +20,7 @@
 
 		if ( is_home() or is_front_page() or is_category() ) : ?>
 			<div class="post-excerpt">
-  			<p><b>TL;DR: </b><?php the_excerpt(); ?></p>
+  			<?php the_excerpt(); ?>
 			</div>
 		<?php endif;
 
