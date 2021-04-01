4be218be1ec2b08902c6e1358e3ab271751a11bf
Kev <kev@quirk.cc>
2021-04-01 15:35:43 +0100

    sdf

--- template-parts/content.php
+++ template-parts/content.php
@@ -20,7 +20,7 @@
 
 		if ( is_home() or is_front_page() or is_category() ) : ?>
 			<div class="post-excerpt">
-  			<?php the_excerpt(); ?>
+  			<b>TL;DR: </b><?php the_excerpt(); ?>
 			</div>
 		<?php endif;
 
