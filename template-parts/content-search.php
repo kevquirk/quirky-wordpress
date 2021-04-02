449fb9a3399594e2b68a9ed85b32b9cfbc1d4d9e
Kev Quirk <kevquirk@users.noreply.github.com>
2021-04-02 22:34:22 +0100

    bgg

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -12,6 +12,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+		
+		<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
@@ -27,7 +31,4 @@
 	<footer class="entry-footer">
 		<p>🏷 <?php nineteen_quirky_entry_footer(); ?></p>
 	</footer><!-- .entry-footer -->
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
 </article><!-- #post-<?php the_ID(); ?> -->
