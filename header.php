48674dbf420384ffd306c44348bc33dc15b4c41f
Kev <kev@quirk.cc>
2021-04-03 17:47:40 +0100

    h1,fdwf

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<!-- Load font -->
-	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() . '/fonts/komika/komika.css' ); ?>">
+	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() . '/fonts/londrina/londrina.css' ); ?>">
 	<!-- Prism syntax highlighting -->
 	<script src="<?php echo esc_url( get_template_directory_uri() . '/js/prism.js' ); ?>"></script>
 	<link rel="stylesheet" href="<?php echo esc_url( get_template_directory_uri() . '/prism.css' ); ?>">
