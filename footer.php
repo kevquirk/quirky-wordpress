fc5a5361a9a3759de8f8de11991d393268514d48
Kev <kev@quirk.cc>
2021-04-06 10:12:06 +0100

    footer

--- footer.php
+++ footer.php
@@ -10,9 +10,8 @@
  */
 
 ?>
-
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 55.25"><defs><style>.cls-1{fill:var(--bg);}</style></defs><title>triangle-uneven-bottom</title><g id="Layer_3" data-name="Layer 3"><polygon class="cls-1" points="0 0 124.49 36.23 500 0 500 55.24 0 55.24 0 0"/></g></svg>
 	<footer id="colophon" class="site-footer clear">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 55.25"><defs><style>.cls-1{fill:var(--bg);}</style></defs><title>triangle-uneven-top</title><g id="Layer_3" data-name="Layer 3"><polygon class="cls-1" points="500 55.24 375.51 19.02 0 55.24 0 0 500 0 500 55.24"/></g></svg>
 		<div class="widget-area">
 			<div class="footer-left">
 				<?php dynamic_sidebar( 'footer_area_one' ); ?>
